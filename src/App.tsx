4cc243c71953731e39089cc5518f0e1cfdfd4f63
Renan Moreira <renan.tsx@gmail.com>
2023-10-29 02:42:45 -0300

    feat(app): add container, sniper and video

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+// import videoSrc from "../public/video/Landscapes.mp4";
+import { Container } from "./components/Container";
+import { Sniper } from "./components/VideoPlayer/Sniper";
+// import { VideoPlayer } from "./components/VideoPlayer";
+
 function App() {
-  return <>APP</>;
+  return (
+    <>
+      <Container bg="blue-500" style={{ aspectRatio: 16 / 9 }}>
+        {/* <VideoPlayer src={videoSrc} /> */}
+        <Sniper speed={1.0} size={4} />
+      </Container>
+    </>
+  );
 }
 
 export default App;
