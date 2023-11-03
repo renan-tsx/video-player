2558838f561338873157848998e1681ae49b276c
Renan Moreira <renan.tsx@gmail.com>
2023-11-03 13:54:53 -0300

    refactor: controller display

--- src/components/VideoPlayer/Controls/index.tsx
+++ src/components/VideoPlayer/Controls/index.tsx
@@ -13,53 +13,18 @@ import { ButtonPlayPause } from "../ButtonPlayPause";
 import { ButtonSkip } from "../ButtonSkip";
 import { ButtonSpeed } from "../ButtonSpeed";
 import { ButtonVolume } from "../ButtonVolume";
-// import { IconPause } from "../IconPause";
-// import { IconPlay } from "../IconPlay";
 import { TimeLine } from "../TimeLine";
 import { Box, Container } from "./styles";
 
 export const Controls = () => {
   const { state } = useVideoPlayer();
-  // const { video, playing, setPlaying, muted, setMuted, time, setTime, buffer } =
-  //   useVideoPlayer();
-
-  // const forward = () => {
-  //   if (!video.current) return;
-  //   video.current.currentTime += 5;
-  // };
-
-  // const backward = () => {
-  //   if (!video.current) return;
-  //   video.current.currentTime -= 5;
-  // };
-
-  // const changePlayBackRate = (speed: number) => {
-  //   if (!video.current) return;
-  //   video.current.playbackRate = speed;
-  // };
-
-  // const mute = () => {
-  //   if (!video.current) return;
-  //   video.current.muted = !video.current.muted;
-  //   setMuted(!video.current.muted);
-  // };
-
-  // const changeVolume = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   if (!video.current) return;
-  //   const volume = Number(e.target.value) / 100;
-  //   video.current.volume = volume;
-  // };
-
-  // const toggleFullScreen = () => {
-  //   if (!video.current) return;
-  //   video.current.requestFullscreen();
-  // };
 
   return (
     <>
       <Container
         className="controls"
         fullScreen={state.fullScreen}
+        playing={state.playing}
         style={{ display: "flex", gap: 0 }}
       >
         <TimeLine />
@@ -75,77 +40,6 @@ export const Controls = () => {
           <ButtonFullScreen />
         </Box>
       </Container>
-      {/* <div className="flex">
-        {playing ? (
-          <button
-            onClick={() => {
-              video.current?.pause();
-              setPlaying(false);
-            }}
-          >
-            <IconPause />
-          </button>
-        ) : (
-          <button
-            onClick={() => {
-              video.current?.play();
-              setPlaying(true);
-            }}
-          >
-            <IconPlay />
-          </button>
-        )}
-
-        <button onClick={backward}>
-          <FaArrowRotateLeft />
-        </button>
-
-        <button onClick={forward}>
-          <FaArrowRotateRight />
-        </button>
-
-        <button onClick={() => changePlayBackRate(1)}>1x</button>
-        <button onClick={() => changePlayBackRate(1.5)}>1.5x</button>
-        <button onClick={() => changePlayBackRate(2)}>2x</button>
-
-        <input
-          type="range"
-          min={0}
-          max={100}
-          defaultValue={50}
-          onChange={changeVolume}
-        />
-
-        <button onClick={mute}>
-          {muted ? <FaVolumeLow /> : <FaVolumeXmark />}
-        </button>
-
-        <button onClick={toggleFullScreen}>
-          <FaExpand />
-        </button>
-      </div>
-      <p>Timeline</p>
-      <div>
-        <input
-          type="range"
-          value={time}
-          min={0}
-          max={video.current?.duration}
-          onChange={(e) => {
-            if (!video.current) return;
-            setTime(+e.target.value);
-            video.current.currentTime = +e.target.value;
-          }}
-        />
-      </div>
-      <p>Buffer</p>
-      <div>
-        <progress
-          value={buffer}
-          max={video.current?.duration}
-          style={{ width: "100%" }}
-        ></progress>
-      </div> */}
     </>
   );
 };
--- src/components/VideoPlayer/Controls/styles.ts
+++ src/components/VideoPlayer/Controls/styles.ts
@@ -2,13 +2,29 @@ import styled from "styled-components";
 
 interface IContainer {
   fullScreen: boolean;
+  playing: boolean;
 }
 
+// full
+// - visible hover
+// - visible not play
+
+// not-full
+// viseble not play
+
 export const Container = styled.div<IContainer>`
-  /* display: flex; */
-  display: ${({ fullScreen }) => (fullScreen ? "flex" : "none")};
-  opacity: ${({ fullScreen }) => (fullScreen ? 1 : 0)};
-  position: ${({ fullScreen }) => (fullScreen ? "fixed" : "absolute")};
+  display: ${({ fullScreen, playing }) => {
+    return fullScreen || !playing ? "flex" : "none";
+  }};
+
+  opacity: ${({ fullScreen, playing }) => {
+    if (fullScreen && playing) return 0;
+    return fullScreen || !playing ? 1 : 0;
+  }};
+
+  position: ${({ fullScreen }) => {
+    return fullScreen ? "fixed" : "absolute";
+  }};
 
   transition: 0.3s;
 
